fd003063cf860351fa4c4e1bf8e2a0120d2c43c8
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-06 18:08:23 -0500

    Delete frontend working; redirect bug

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,6 +1,23 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 export const BlogPost = (props) => {
+    const [name, setName] = useState(props.name)
+    const [author, setAuthor] = useState(props.author)
+    const [content, setContent] = useState(props.content)
+
+    const deletePost = (e) => {
+        e.preventDefault()
+        axios.delete(`http://localhost:4000/blog/${props._id}`, {
+            name: name,
+            author: author,
+            content: content
+        })
+        .then(() => window.location = '/blog')
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className="container pt-3">
             <h1>{props.name}</h1>
@@ -14,6 +31,7 @@ export const BlogPost = (props) => {
                 <Link
                     to={`/blog/${props._id}/edit`}>
                     <button className="btn btn-dark">Edit Post</button>
+                    <button onClick={deletePost} className="btn btn-dark">Delete Post</button>
                 </Link>
             </div>
         </div>
