b803c9baaa31cb8d7968f2d2b228562e963ed840
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-06 18:02:43 -0500

    Create New Post frontend working.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,31 +1,52 @@
 import { useState } from 'react'
 import axios from 'axios'
-import { useHistory } from 'react-router-dom'
 
-export const NewPost = () => {
-    const history = useHistory()
-    const [newPost, setNewPost] = useState({
-        name: "",
-        author: "",
-        content: ""
-    })
+export const NewPost = (props) => {
+    const [name, setName] = useState("")
+    const [author, setAuthor] = useState("")
+    const [content, setContent] = useState("")
+
+    const createPost = (e) => {
+        e.preventDefault()
+        axios.post(`http://localhost:4000/blog`, {
+            name: name,
+            author: author,
+            content: content
+        })
+        .then(() => window.location = '/blog')
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className="container pt-3">
             <h1>New Incredible True Post</h1>
             <div className="wrapper pt-3">
-                <form id="new-form">
+                <form id="new-form" onSubmit={createPost}>
                     <div className="form-group">
                         <label for="name">Name</label>
-                        <input type="text" className="form-control" name="name" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            placeholder={props.name}/>
                     </div>
                     <div className="form-group">
                         <label for="name">Author</label>
-                        <input type="text" className="form-control" name="author"/>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={author}
+                            onChange={e => setAuthor(e.target.value)}
+                            placeholder={props.author}/>
                     </div>
                     <div className="form-group">
                         <label for="content">Content</label>
-                        <textarea className="form-control" name="content"/>
+                        <textarea 
+                            className="form-control" 
+                            value={content}
+                            onChange={e => setContent(e.target.value)}
+                            placeholder={props.content}/>
                     </div>
                     <button className="btn btn-dark">Create</button>
                 </form>
